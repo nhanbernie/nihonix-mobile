62cff4258d4f55df5a98cfc46ce946d758ca5d04
nhanbernie <ttnhanbernie@gmail.com>
2025-10-05 14:28:46 +0700

    chore: base structure for nihonix flutter

---
+++ lib/test/download_file.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+Future<File> downloadToFile(String url, File out) async {
+  final client = HttpClient();
+  try {
+    final uri = Uri.parse(url);
+    final req = await client.getUrl(uri);
+    final res = await req.close();
+
+    if (res.statusCode != HttpStatus.ok) {
+      throw HttpException('HTTP ${res.statusCode}', uri: uri);
+    }
+
+    final sink = out.openWrite(); // StreamSink<List<int>>
+    final total = res.contentLength; // có thể là -1 nếu server không báo
+    var received = 0;
+
+    await for (final chunk in res) {
+      received += chunk.length;
+      sink.add(chunk);
+      // print progress
+      if (total > 0) {
+        final p = ((received / total) * 100).toStringAsFixed(1);
+        // print('Progress: $p%');
+      }
+    }
+    await sink.flush();
+    await sink.close();
+    return out;
+  } finally {
+    client.close();
+  }
+}

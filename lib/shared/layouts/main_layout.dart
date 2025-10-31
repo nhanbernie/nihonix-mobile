dfb5b49385132ae1ca4c9980de0a286e63a8279d
tnhanbernie <ttnhanbernie@gmail.com>
2025-10-31 16:05:51 +0700

    refactor: buttom navbar

--- lib/shared/layouts/main_layout.dart
+++ lib/shared/layouts/main_layout.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-import 'package:nihonix/core/constants/app_strings.dart';
 import 'package:nihonix/core/router/route_constants.dart';
+import 'package:nihonix/shared/widgets/app_bottom_nav_bar.dart';
 
 const routes = [
   AppRoutes.home,
@@ -24,47 +24,10 @@ class MainLayout extends StatelessWidget {
     final showBottomNav = currentIndex != -1;
 
     return Scaffold(
-      body: SafeArea(
-        child: child,
-      ),
-      bottomNavigationBar: showBottomNav
-          ? Container(
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withValues(alpha: 0.1),
-                    blurRadius: 10,
-                    offset: const Offset(0, -2),
-                  ),
-                ],
-              ),
-              child: BottomNavigationBar(
-                elevation: 0,
-                currentIndex: currentIndex != -1 ? currentIndex : 0,
-                onTap: (i) {
-                  switch (i) {
-                    case 0:
-                      context.go(AppRoutes.home);
-                      break;
-                    case 1:
-                      context.go(AppRoutes.lesson);
-                      break;
-                    case 2:
-                      context.go(AppRoutes.profile);
-                      break;
-                  }
-                },
-                items: [
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.home), label: AppStrings.home),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.book), label: AppStrings.lesson),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.person), label: AppStrings.profile),
-                ],
-              ),
-            )
-          : null,
+      extendBody: true,
+      body: child,
+      bottomNavigationBar:
+          showBottomNav ? AppBottomNavBar(currentIndex: currentIndex) : null,
     );
   }
 }
---
+++ lib/shared/widgets/app_bottom_nav_bar.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import 'package:nihonix/core/constants/app_strings.dart';
+import 'package:nihonix/core/router/route_constants.dart';
+
+class AppBottomNavBar extends StatelessWidget {
+  final int currentIndex;
+
+  const AppBottomNavBar({
+    super.key,
+    required this.currentIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.1),
+            blurRadius: 10,
+            offset: const Offset(0, -2),
+          ),
+        ],
+      ),
+      child: BottomNavigationBar(
+        elevation: 0,
+        currentIndex: currentIndex,
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              context.go(AppRoutes.home);
+              break;
+            case 1:
+              context.go(AppRoutes.lesson);
+              break;
+            case 2:
+              context.go(AppRoutes.profile);
+              break;
+          }
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: AppStrings.home,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: AppStrings.lesson,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: AppStrings.profile,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
